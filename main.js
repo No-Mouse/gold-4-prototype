ccc4aef67e8fb1af0e14cc173d427f43656a5af1
No-Mouse <35830636+No-Mouse@users.noreply.github.com>
2018-01-26 23:54:55 -0800

    Test2

--- main.js
+++ main.js
@@ -9,11 +9,9 @@
 function GameBoard(game) {
     Entity.call(this, game, 20, 20);
 	this.score = 0;
-	this.money = 1000;
+	this.money = 100;
     this.grid = false;
     this.canBuy = true;
-    this.shadow = false;
-
     this.player = 0;
     this.board = [];
     for (var i = 0; i < 22; i++) {
@@ -26,6 +24,17 @@ function GameBoard(game) {
 
 GameBoard.prototype = new Entity();
 GameBoard.prototype.constructor = GameBoard;
+
+
+//Will use this later to determine if the place on the map is a path for enemies or not
+//Not this logic is in correct place. Should be a field of gameBoard
+GameBoard.prototype.getPlaceTower = function() {
+    var canPlace = false;
+    if(this.board[this.game.click.x][this.game.click.y] === 0) {
+        canPlace = true;
+    }
+    return canPlace;
+}
 
 GameBoard.prototype.update = function () {
     if (this.game.click 
@@ -53,6 +62,7 @@ GameBoard.prototype.update = function () {
             canAfford = true;
         }	
 
+        //TODO: fix to be accurate to specific tower: EX: start with 100 use 75 tower, have 25 left but cant place
         if(this.money == 0) {
             this.canBuy = false;
         }
@@ -131,13 +141,13 @@ GameBoard.prototype.draw = function (ctx) {
 
 
 // the "main" code begins here
-
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
 ASSET_MANAGER.queueDownload("./img/black.png");
 ASSET_MANAGER.queueDownload("./img/white.png");
 ASSET_MANAGER.queueDownload("./img/tower3.png");
+ASSET_MANAGER.queueDownload("./img/Attack.png");
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
@@ -146,8 +156,77 @@ ASSET_MANAGER.downloadAll(function () {
 
     var gameEngine = new GameEngine();
     var gameboard = new GameBoard(gameEngine);
+    console.log("GAME ENGINE " + gameEngine);
+
+
+    //console.log(gameboard);
     gameEngine.addEntity(gameboard);
- 
     gameEngine.init(ctx);
+    var attacker = new attackDude(gameEngine, ASSET_MANAGER.getAsset("./img/Attack.png"));
+    gameEngine.addEntity(attacker);
     gameEngine.start();
 });
+
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
+    this.spriteSheet = spriteSheet;
+    this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.scale = scale;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
+
+    ctx.drawImage(this.spriteSheet,
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth * this.scale,
+                 this.frameHeight * this.scale);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+function attackDude(game, spritesheet) {
+    Entity.call(this, game, 0, 0);
+	this.animation = new Animation(spritesheet, 536, 495, 10, 0.10, 10, true, 0.2);
+	this.x = 5;
+    this.y = 5;
+    this.speed = 100;
+    this.game = game;
+    this.ctx = game.ctx;
+    //console.log(this.ctx);
+}
+
+attackDude.prototype = new Entity();
+attackDude.prototype.constructor = attackDude;
+
+attackDude.prototype.draw = function () {
+	this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+}
+
+attackDude.prototype.update = function () {
+    if (this.animation.elapsedTime < this.animation.totalTime * 8 / 14)
+        this.x += this.game.clockTick * this.speed;
+    if (this.x > 800) this.x = -230;
+}
